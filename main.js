104455c8b3c33d9ff6b91f0e2a3f9cd6f82dda6c
bekhruz656 <madrimovbekhruz00@gmail.com>
2024-03-13 18:06:47 +0500

    this is my project js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let a = String(prompt("Enter first value"));
 let b = prompt("Enter second value");
 let c = Boolean(prompt("Enter third value"))
-alert("Siz kiritgan malumot togriligiga aminmisiz" + typeof(a))
+alert("Siz kiritgan malumot togriligiga aminmisiz")
